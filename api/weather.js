bc126c34d741232eabd4529b41c881bc0ab6c0b6
Farzan Ahmad <farzansidd07@gmail.com>
2025-08-04 17:43:18 +0530

    weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const city = req.query.city;
-  const API_KEY = "78afb45c81c660fb53428473251177bc";
+  const API_KEY = "78afb45c81c660fb53428473251177bc"; 
 
   const url = https://api.openweathermap.org/data/2.5/weather?units=metric&q=
 
